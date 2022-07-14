889454ce1963b349b2956564b2cc5d037bc7240d
wytxer <wy@bszhct.com>
2022-07-14 17:30:22 +0800

    refactor：测试用例更新

--- test/app/controller/user.test.js
+++ test/app/controller/user.test.js
@@ -5,17 +5,17 @@ const { app, assert } = require('egg-mock/bootstrap')
 describe('test/app/controller/user.test.js', () => {
   it('mock 登录', () => {
     return app.httpRequest()
-      .get('/api/user/mock?id=1')
+      .get('/api/v1/user/mock?id=1000')
       .type('json')
       .expect(200)
       .expect(res => {
-        assert(res.body.code === 0 && res.body.data.id === 1)
+        assert(res.body.code === 0 && res.body.data.id === 1000)
       })
   })
 
   it('获取用户信息', () => {
     return app.httpRequest()
-      .post('/api/user/info')
+      .post('/api/v1/user/info')
       .type('json')
       .expect(200)
       .expect(res => {
--- test/app/service/user.test.js
+++ test/app/service/user.test.js
@@ -5,8 +5,8 @@ const { app, assert } = require('egg-mock/bootstrap')
 describe('test/app/controller/home.test.js', () => {
   it('获取用户信息', async () => {
     const ctx = app.mockContext()
-    const user = await ctx.service.user.info(1)
+    const user = await ctx.service.user.info(1000)
     assert(user)
-    assert(user.id === 1)
+    assert(user.id === 1000)
   })
 })
