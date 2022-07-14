3bc38d3cc2b49c3d36d81e7b4c18a9dc4efc2461
wytxer <wy@bszhct.com>
2022-07-14 16:37:15 +0800

    refactor：查询优化

--- app/service/user.js
+++ app/service/user.js
@@ -8,22 +8,32 @@ const attributes = [ 'id', 'openId', 'nickName', 'avatarUrl', 'phone', 'loggedAt
 
 class UserService extends Service {
   // 登录
-  async login(openId, values) {
+  async login(values) {
     const { model } = this.ctx
 
-    return await model.User.findOne({
-      where: { openId },
-      attributes
-    })
-      .then(async res => {
-        const loggedAt = moment().format('YYYY-MM-DD HH:mm:ss')
-        if (res) {
-          return await res.update({ loggedAt })
+    // 先根据 unionId 查找用户是否存在
+    return await model.User.findOne({ where: { unionId: values.unionId }, attributes })
+      .then(async user => {
+        // 如果未找到，尝试使用 openId 查找用户是否存在
+        if (!user) {
+          user = await model.User.findOne({ where: { openId: values.openId }, attributes })
         }
-        values.loggedAt = loggedAt
-        return await model.User.create(values)
+        let newUser
+        // 如果找到了用户，更新用户信息
+        if (user) {
+          newUser = user
+          await model.User.update(values, { where: { id: user.id } })
+        } else {
+          // 否则新增一个用户
+          newUser = await model.User.create(values)
+        }
+        // 更新用户登录时间
+        const loggedAt = moment().format('YYYY-MM-DD HH:mm:ss')
+        await model.User.update({ loggedAt }, { where: { id: newUser.id } })
+        // 返回最新的用户信息
+        return await model.User.findOne({ where: { id: newUser.id }, attributes })
+          .then(res => res.toJSON())
       })
-      .then(res => res.toJSON())
   }
 
   // 保存手机号码
@@ -31,10 +41,16 @@ class UserService extends Service {
     const { model } = this.ctx
 
     return await model.User.findOne({
-      where: { id }
+      where: { id },
+      attributes: [ 'id', 'phone' ]
     })
       .then(async res => {
-        if (res) return await res.update({ phone })
+        if (res) {
+          const loggedAt = moment().format('YYYY-MM-DD HH:mm:ss')
+          await res.update({ phone, loggedAt })
+          return await model.User.findOne({ where: { id: res.id }, attributes })
+            .then(res => res.toJSON())
+        }
         return false
       })
   }
@@ -50,10 +66,13 @@ class UserService extends Service {
       .then(async res => {
         if (res) {
           // 更新登录时间
-          return await res.update({
-            loggedAt: moment().format('YYYY-MM-DD HH:mm:ss')
+          const loggedAt = moment().format('YYYY-MM-DD HH:mm:ss')
+          await res.update({ loggedAt })
+          return await model.User.findOne({
+            where: { id },
+            attributes
           })
-            .then(res => res.toJSON())
+            .then(user => user.toJSON())
         }
         return false
       })
