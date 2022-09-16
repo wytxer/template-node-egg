3b25ba4c91eaaab33dd50668d7c1074000393628
wytxer <wy@bszhct.com>
2022-09-16 11:26:51 +0800

    feat：新增获取手机号方法

--- app/service/wechatApp.js
+++ app/service/wechatApp.js
@@ -2,6 +2,15 @@
 
 const Service = require('egg').Service
 
+/**
+ * 微信小程序
+ */
+
+// 缓存鉴权信息
+let wechatAccessToken = {
+  accessToken: null, invalidTime: 0
+}
+
 class WechatAppService extends Service {
   constructor(ctx) {
     super(ctx)
@@ -20,18 +29,67 @@ class WechatAppService extends Service {
   // 官方文档：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/login/auth.code2Session.html
   async code2Session(code) {
     const { ctx } = this
-    const { appId, secret } = ctx.app.config.wxapp
+    const { helper, logger } = ctx
+    const { appId, secret } = ctx.app.config.wechatApp
+
     return await this.request(`/sns/jscode2session?appid=${appId}&secret=${secret}&js_code=${code}&grant_type=authorization_code`)
       .then(res => res.data)
       // 只返回需要的数据
       .then(res => {
-        ctx.logger.info('微信登录凭证校验：%s', ctx.helper.stringify(res))
+        logger.info('微信登录凭证校验：%s', helper.stringify(res))
         if (res.openid) {
           return {
-            openId: res.openid, sessionKey: res.session_key, unionId: res.unionid
+            openId: res.openid, sessionKey: res.session_key, unionId: res.unionid, appId
           }
         }
-        return Promise.reject(new Error(ctx.helper.stringify(res)))
+        return Promise.reject(new Error(helper.stringify(res)))
+      })
+  }
+
+  // 获取小程序全局唯一后台接口调用凭据
+  // 官方文档：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/access-token/auth.getAccessToken.html
+  async accessToken() {
+    const { ctx } = this
+    const { helper, logger } = ctx
+    const { appId, secret } = ctx.app.config.wechatApp
+
+    // 先从缓存从读取
+    if (wechatAccessToken.invalidTime - new Date().getTime() > 0) {
+      return wechatAccessToken
+    }
+
+    return await this.request(`/cgi-bin/token?grant_type=client_credential&appid=${appId}&secret=${secret}`)
+      .then(res => res.data)
+      // 只返回需要的数据
+      .then(res => {
+        logger.info('获取小程序全局唯一后台接口调用凭据：%s', helper.stringify(res))
+        if (res.access_token) {
+          wechatAccessToken = {
+            accessToken: res.access_token, invalidTime: new Date().getTime() + (res.expires_in * 1000)
+          }
+          return wechatAccessToken
+        }
+        return Promise.reject(new Error(helper.stringify(res)))
+      })
+  }
+
+  // 获取授权手机号
+  // 官方文档：https://developers.weixin.qq.com/miniprogram/dev/api-backend/open-api/phonenumber/phonenumber.getPhoneNumber.html
+  async getPhoneNumber(accessToken, code) {
+    const { helper, logger } = this.ctx
+
+    return await this.request(`/wxa/business/getuserphonenumber?access_token=${accessToken}`, {
+      method: 'POST',
+      data: { code }
+    })
+      .then(res => res.data)
+      // 只返回需要的数据
+      .then(res => {
+        logger.info('获取授权手机号：%s', helper.stringify(res))
+        if (res.errcode === 0) {
+          return res.phone_info
+        }
+        return Promise.reject(new Error(helper.stringify(res)))
       })
   }
 }
